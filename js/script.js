fe88ff299baa28dc2424f25ccc950a553d661fb9
Alex <110024503+OleksandraMedvedenko@users.noreply.github.com>
2023-08-24 18:18:14 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu__list--active');
+})
+
+const productsSwiper = document.querySelector('.swiper-products');
+
+if (productsSwiper) {
+    const swiper = new Swiper('.swiper-products', {
+        slidesPerView: 3,
+        autoHeight: true,
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+    });
+}
