2517708a3d483a1d67fbf8ffad2e759fd14d3ed9
Alex <110024503+OleksandraMedvedenko@users.noreply.github.com>
2023-08-25 15:28:05 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const menuBtn = document.querySelector('.menu__btn');
-const menu = document.querySelector('.menu__list');
+const menu = document.querySelector ('.menu__list');
 
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('menu__list--active');
@@ -15,36 +15,18 @@ if (productsSwiper) {
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
-        }
-    });
-    window.addEventListener('resize', () => {
-        if (window.innerWidth <= 768 && swiper.params.slidesPerView !== 2) {
-            swiper.destroy(); // Уничтожьте текущий экземпляр свайпера
-            swiper = new Swiper('.swiper-products', {
-                slidesPerView: 2,
-                autoHeight: true,
-                loop: true,
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                },
-            });
-        } else if (window.innerWidth > 768 && swiper.params.slidesPerView !== 3) {
-            swiper.destroy();
-            swiper = new Swiper('.swiper-products', {
-                slidesPerView: 3,
-                autoHeight: true,
-                loop: true,
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                },
-                breakpoints: {
-                    768: {
-                        slidesPerView: 2, 
-                    }
-                }
-            });
+        },
+        breakpoints: {
+            320:{
+                slidesPerView:2
+            },
+            768: {
+                slidesPerView: 2
+            },
+            1024: {
+                slidesPerView: 3
+            }
         }
     });
 }
+
