2d2a2dd8c488f95501740bfdd8e752fdc4889e1c
Alex <110024503+OleksandraMedvedenko@users.noreply.github.com>
2023-08-26 17:13:43 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -42,4 +42,5 @@ const menu = document.querySelector ('.menu__list');
 
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('menu__list--active')
+    menuBtn.classList.toggle('active')
 })
